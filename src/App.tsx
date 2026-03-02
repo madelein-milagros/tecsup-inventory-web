8f3d08c7116a2f3c983c8b2ce13ea34d2618e8b0
JosepRivera <josepdanton1518@gmail.com>
2026-03-02 09:58:56 -0500

    feat: update App component structure and enhance Vite configuration with Tailwind CSS support

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
-import { BottomNav } from "@/components/shared/BottomNav";
-import { AppRouter } from "@/router";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <BottomNav />
-      <AppRouter />
     </BrowserRouter>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import fs from "node:fs";
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    https: {
+      cert: fs.readFileSync("192.168.18.243.pem"),
+      key: fs.readFileSync("192.168.18.243-key.pem"),
+    },
+    host: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("lucide-react")) return "lucide";
+            if (id.includes("radix-ui")) return "radix";
+            if (id.includes("react")) return "react-vendor";
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
